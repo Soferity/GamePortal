09df2a17d03979c06d3d01c7b0398e9846686a86
Taiizor <41683699+Taiizor@users.noreply.github.com>
2022-09-04 06:14:36 +0300

    Infrastructure Preparation 2

---
+++ GamePortal.Winforms/Form1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components.WebView.WindowsForms;
+using Microsoft.Extensions.DependencyInjection;
+using System.Windows.Forms;
+
+namespace GamePortal.Winforms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var services = new ServiceCollection();
+            services.AddWindowsFormsBlazorWebView();
+            blazorWebView1.HostPage = "wwwroot\\index.html";
+            blazorWebView1.Services = services.BuildServiceProvider();
+            blazorWebView1.RootComponents.Add<Counter>("#app");
+        }
+    }
+}
