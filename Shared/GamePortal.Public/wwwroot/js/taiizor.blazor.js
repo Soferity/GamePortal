addda1c448fd0f12f44173118de1ceda4225b394
Taiizor <41683699+Taiizor@users.noreply.github.com>
2022-09-08 17:15:58 +0300

    Altyapı Hazırlığı 12

--- Shared/GamePortal.Public/wwwroot/js/taiizor.blazor.js
+++ Shared/GamePortal.Public/wwwroot/js/taiizor.blazor.js
@@ -1,42 +1,42 @@
-﻿function Reload() {
+function Reload() {
 	window.location.reload();
 }
 
-function GoLocation(uriLocation = "/") {
-	location.href = uriLocation;
-	//window.location.href = uriLocation;
+function GoLocation(uriLocation = '/') {
+    location.href = uriLocation;
+    //window.location.href = uriLocation;
 }
 
 function ReloadElement(elementId, methodType) {
-	if (methodType === true) {
-		var container = document.getElementById(elementId);
-		var content = container.innerHTML;
-		container.innerHTML = content;
-	}
-	else {
-		document.getElementById(elementId).innerHTML = document.getElementById(elementId).innerHTML;
-	}
+    if (methodType === true) {
+        var container = document.getElementById(elementId);
+        var content = container.innerHTML;
+        container.innerHTML = content;
+    }
+    else {
+        document.getElementById(elementId).innerHTML = document.getElementById(elementId).innerHTML;
+    }
 }
 
 function IncludeFont(path, rel) {
-	var customFont = document.createElement('link');
-	customFont.setAttribute('href', path);
-	customFont.setAttribute('rel', rel);
-	document.head.appendChild(customFont);
+    var customFont = document.createElement('link');
+    customFont.setAttribute('href', path);
+    customFont.setAttribute('rel', rel);
+    document.head.appendChild(customFont);
 }
 
 function IncludeJs(path) {
 	var customScript = document.createElement('script');
-	customScript.setAttribute('src', path);
-	document.body.appendChild(customScript);
+    customScript.setAttribute('src', path);
+    document.body.appendChild(customScript);
 }
 
 function IncludeCss(path, rel, type) {
-	var customStylesheet = document.createElement('link');
-	customStylesheet.setAttribute('href', path);
-	customStylesheet.setAttribute('rel', rel);
-	customStylesheet.setAttribute('type', type);
-	document.head.appendChild(customStylesheet);
+    var customStylesheet = document.createElement('link');
+    customStylesheet.setAttribute('href', path);
+    customStylesheet.setAttribute('rel', rel);
+    customStylesheet.setAttribute('type', type);
+    document.head.appendChild(customStylesheet);
 }
 
 function SetClass(classId, className) {
@@ -56,24 +56,53 @@ function RemoveClass(classId, className) {
 	document.getElementById(classId).classList.remove(className);
 }
 
-function InvokeMethodAsync(assemblyName, methodName, arguments) {
-	if (arguments != null && arguments.length > 0) {
-		return DotNet.invokeMethodAsync(assemblyName, methodName, arguments);
-	}
-	else {
+function SetStyle(styleId, stylePair) {
+    document.getElementById(styleId).style = stylePair;
+}
 
-		return DotNet.invokeMethodAsync(assemblyName, methodName);
-	}
+function AddStyle(styleId, styleName, styleValue) {
+    document.getElementById(styleId).style.setProperty(styleName, styleValue);
+}
+
+var styles = [];
+
+function ToggleStyle(styleId) {
+    var style = document.getElementById(styleId).style.cssText;
+
+    if (style != '') {
+        styles[styleId] = document.getElementById(styleId).style.cssText;
+        document.getElementById(styleId).style.cssText = null;
+        console.log("1");
+    }
+    else if (styles[styleId] != null) {
+        document.getElementById(styleId).style.cssText = styles[styleId];
+        styles[styleId] = null;
+        console.log("1");
+    }
+}
+
+function RemoveStyle(styleId, styleName) {
+    document.getElementById(styleId).style.removeProperty(styleName);
+}
+
+function InvokeMethodAsync(assemblyName, methodName, arguments) {
+    if (arguments != null && arguments.length > 0) {
+        return DotNet.invokeMethodAsync(assemblyName, methodName, arguments);
+    }
+    else {
+
+        return DotNet.invokeMethodAsync(assemblyName, methodName);
+    }
 }
 
 function InvokeReferenceMethodAsync(objectReference, assemblyName, methodName, arguments) {
-	if (arguments != null && arguments.length > 0) {
-		objectReference.invokeMethodAsync(assemblyName, methodName, arguments);
-	}
-	else {
+    if (arguments != null && arguments.length > 0) {
+        objectReference.invokeMethodAsync(assemblyName, methodName, arguments);
+    }
+    else {
 
-		objectReference.invokeMethodAsync(assemblyName, methodName);
-	}
-	
-	//objectReference.dispose();
+        objectReference.invokeMethodAsync(assemblyName, methodName);
+    }
+    
+    //objectReference.dispose();
 }
