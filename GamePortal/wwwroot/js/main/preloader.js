be395a647a9044c1dabc38a18646369af8632e77
Taiizor <41683699+Taiizor@users.noreply.github.com>
2022-09-10 04:13:29 +0300

    Preloader İyileştirmesi

---
+++ GamePortal/wwwroot/js/main/preloader.js
@@ -0,0 +1,37 @@
+window.onload = function () {
+    //Can also use window.addEventListener('load', (event) => {
+    Preloaded();
+};
+
+document.addEventListener('readystatechange', event => {
+    switch (document.readyState) {
+        case "loading":
+            //None
+            break;
+        case "interactive":
+            //First
+            Preloaded();
+            break;
+        case "complete":
+            //Last
+            Preloaded();
+            break;
+        default:
+            //None
+            break;
+    }
+});
+
+var countLoader = 0;
+
+function Preloaded() {
+    if (countLoader > 2) {
+        countLoader = 0;
+        document.getElementById("preloader").style.display = "none";
+        document.getElementById("application").style.visibility = "visible";
+        document.getElementById("body").style.overflow = "unset";
+    }
+    else {
+        ++countLoader;
+    }
+}
