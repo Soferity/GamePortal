6a619975c8b1cbc0ffdb0278458a04357cbddd2a
Taiizor <41683699+Taiizor@users.noreply.github.com>
2022-10-22 23:41:52 +0300

    Ufak Düzeltmeler

--- GamePortal/Value/AppValue.cs
+++ GamePortal/Value/AppValue.cs
@@ -1,12 +1,12 @@
 ﻿using GamePortal.Helper;
 using Taiizor.Essentials.Maui.Enum;
-using HP = Taiizor.Essentials.Maui.Helper.Platform;
+using EP = Taiizor.Essentials.Maui.Extension.Platform;
 
 namespace GamePortal.Value
 {
     internal class AppValue
     {
-        public static PlatformEnum Platform = HP.GetPlatform;
+        public static PlatformEnum Platform = EP.Type;
 
         public static BuildEnum Build = HelperBuild.GetBuild;
     }
