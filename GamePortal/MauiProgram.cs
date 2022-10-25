7542a1b6c30ce613d16bbf129a96727246318a4e
Taiizor <41683699+Taiizor@users.noreply.github.com>
2022-10-25 23:04:31 +0300

    Ufak Düzeltmeler

--- GamePortal/MauiProgram.cs
+++ GamePortal/MauiProgram.cs
@@ -1,6 +1,5 @@
 ﻿using GamePortal.Manage;
-using Microsoft.Maui.LifecycleEvents;
-using System.Diagnostics;
+using Taiizor.Essentials.Maui.AppCenter.Extension;
 using Taiizor.Essentials.Maui.AppCenter.Services;
 using Taiizor.Essentials.Maui.Cross;
 using Taiizor.Essentials.Maui.Interfaces;
@@ -30,60 +29,16 @@ namespace GamePortal
 
             builder.Services.AddSingleton<IDialogService, DialogService>();
 
-            builder.ConfigureLifecycleEvents(events =>
-            {
-
-#if WINDOWS
-			events.AddWindows(windows => windows
-                .OnActivated((window, args) => LogEvent("OnActivated", $"{args}"))
-                .OnClosed((window, args) => LogEvent("OnClosed", $"{args}")) //End
-                .OnLaunched((window, args) => LogEvent("OnLaunched", $"{args}"))
-                .OnLaunching((window, args) => AppCenter()) //Start //LogEvent("OnLaunching", $"{args}")
-                .OnWindowCreated((window) => LogEvent("OnWindowCreated"))
-                .OnResumed((window) => LogEvent("OnResumed"))
-                .OnVisibilityChanged((window, args) => LogEvent("OnVisibilityChanged", $"{args}")));
-#elif ANDROID
-            events.AddAndroid(android => android
-                .OnActivityResult((activity, requestCode, resultCode, data) => LogEvent("OnActivityResult", $"{requestCode}, {resultCode}, {data}"))
-                .OnStart((activity) => LogEvent("OnStart"))
-                .OnCreate((activity, bundle) => AppCenter()) //Start //LogEvent("OnCreate", $"{bundle}")
-                .OnBackPressed((activity) => LogEvent("OnBackPressed"))
-                .OnStop((activity) => LogEvent("OnStop"))); //End
-#elif IOS || MACCATALYST
-            events.AddiOS(ios => ios
-                .OnActivated((app) => LogEvent("OnActivated"))
-                .OnResignActivation((app) => LogEvent("OnResignActivation"))
-                .DidEnterBackground((app) => LogEvent("DidEnterBackground"))
-                .FinishedLaunching((app, dict) => AppCenter()) //Start //LogEvent("FinishedLaunching", $"{dict}")
-                .WillTerminate((app) => LogEvent("WillTerminate"))); //End
-#endif
-
-            });
-
-            //Taiizor.Essentials.Maui.Extension.Culture.Set("en-GB");
-
-            return builder.Build();
-        }
-
-        private static bool LogEvent(string eventName, string type = null)
-        {
-            Debug.WriteLine($"Lifecycle Event: {eventName}{(type == null ? string.Empty : $" ({type})")}");
-
-            return true;
-        }
-
-        private static bool AppCenter()
-        {
-            AppCenterService.Engine(General.Keys, General.Watch);
+            builder.UseAppCenter(General.Keys, General.Watch);
 
             CrossException.UnhandledException += (sender, args) =>
             {
                 AppCenterService.Exception(args);
             };
 
-            Debug.WriteLine($"Lifecycle Event: AppCenter");
+            //Taiizor.Essentials.Maui.Extension.Culture.Set("en-GB");
 
-            return true;
+            return builder.Build();
         }
     }
 }
