04b6e074f429148343cc887ed4128c14c671fd5c
Taiizor <41683699+Taiizor@users.noreply.github.com>
2022-12-10 21:24:03 +0300

    Update General.cs

--- src/GamePortal/Manage/General.cs
+++ src/GamePortal/Manage/General.cs
@@ -1,6 +1,5 @@
 ﻿using Taiizor.Essentials.Blazor.Enum;
 using Taiizor.Essentials.Maui.AppCenter.Enum;
-using Taiizor.Essentials.Maui.AppCenter.Helper;
 using Taiizor.Essentials.Maui.AppCenter.Struct;
 using Taiizor.Essentials.Maui.Extension;
 using Taiizor.Essentials.Maui.Struct;
@@ -26,10 +25,6 @@ namespace GamePortal.Manage
         public static Random Random = new();
 
         public static string Title = Project.Name;
-
-        public const string DebugToken = "GAMEPORTAL_DEBUG_{0}_TARGET_TOKEN";
-
-        public const string ReleaseToken = "GAMEPORTAL_RELEASE_{0}_TARGET_TOKEN";
 
         public static WatchStruct Watch = new()
         {
@@ -70,15 +65,15 @@ namespace GamePortal.Manage
         {
         
 #if DEBUG
-            { ACEAE.iOS, Key.GetVariable(ACEAE.iOS, DebugToken) },
-            { ACEAE.macOS, Key.GetVariable(ACEAE.macOS, DebugToken) },
-            { ACEAE.Android, Key.GetVariable(ACEAE.Android, DebugToken) },
-            { ACEAE.Windows, Key.GetVariable(ACEAE.Windows, DebugToken) }
+            { ACEAE.iOS, "" },
+            { ACEAE.macOS, "" },
+            { ACEAE.Android, "" },
+            { ACEAE.Windows, "" }
 #else
-            { ACEAE.iOS, Key.GetVariable(ACEAE.iOS, ReleaseToken) },
-            { ACEAE.macOS, Key.GetVariable(ACEAE.macOS, ReleaseToken) },
-            { ACEAE.Android, Key.GetVariable(ACEAE.Android, ReleaseToken) },
-            { ACEAE.Windows, Key.GetVariable(ACEAE.Windows, ReleaseToken) }
+            { ACEAE.iOS, "" },
+            { ACEAE.macOS, "" },
+            { ACEAE.Android, "" },
+            { ACEAE.Windows, "" }
 #endif
 
         };
