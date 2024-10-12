aed34d4015b3c96bf89723bd8c158761770dc152
Taiizor <41683699+Taiizor@users.noreply.github.com>
2024-10-13 00:40:43 +0300

    Update Code of Conducts

--- src/GamePortal/Platforms/Android/MainApplication.cs
+++ src/GamePortal/Platforms/Android/MainApplication.cs
@@ -6,8 +6,7 @@ namespace GamePortal;
 [Application]
 public class MainApplication : MauiApplication
 {
-    public MainApplication(IntPtr handle, JniHandleOwnership ownership)
-        : base(handle, ownership)
+    public MainApplication(IntPtr handle, JniHandleOwnership ownership) : base(handle, ownership)
     {
     }
 
